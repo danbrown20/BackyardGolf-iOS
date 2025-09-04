d9eb9810f57bad0f332c33fe4964eadf73a84d45
danbrown20 <danbrown20@users.noreply.github.com>
2025-09-04 17:27:44 -0500

    Initial Commit

---
+++ BackyardGolf/BackyardGolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackyardGolfApp.swift
+//  BackyardGolf
+//
+//  Created by Dan on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BackyardGolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackyardGolfTests/BackyardGolfTests.swift
@@ -0,0 +1,17 @@
+//
+//  BackyardGolfTests.swift
+//  BackyardGolfTests
+//
+//  Created by Dan on 9/4/25.
+//
+
+import Testing
+@testable import BackyardGolf
+
+struct BackyardGolfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
